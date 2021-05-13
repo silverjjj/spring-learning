2905fff82e338ec62a632c0ee87269bddb04618b
이은재 <silver_jae@naver.com>
2021-05-13 16:28:29 +0900

    H2 connect

--- hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,9 +1,12 @@
 package hello.hellospring;
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 /**
  * 스프링 빈이 등륵할때 어노테이션이 아닌 직접 자바로 등록하는 방법
@@ -14,15 +17,25 @@ import org.springframework.context.annotation.Configuration;
  * helloController ==> memberService ==> memberRepository 이렇게 스프링 빈이 등록된다.
  */
 
-//@Configuration
-//public class SpringConfig {
-//    @Bean
-//    public MemberService memberService() {
-//        return new MemberService(memberRepository());
-//    }
-//
-//    @Bean
-//    public MemberRepository memberRepository() {
+@Configuration
+public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
-//    }
-//}
+        return new JdbcMemberRepository(dataSource);
+    }
+}
